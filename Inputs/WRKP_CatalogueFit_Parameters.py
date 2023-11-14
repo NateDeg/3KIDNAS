9409d5e7e76324468fc7d8acb127f29c9486ff7e
NateDeg <30897649+NateDeg@users.noreply.github.com>
2023-11-14 11:49:36 -0500

    Added KinTR to input file and set it to testing values

--- Inputs/WRKP_CatalogueFit_Parameters.py
+++ Inputs/WRKP_CatalogueFit_Parameters.py
@@ -6,12 +6,12 @@ CatName="/Users/nate/Dropbox/WALLABY/NGC4808_TR1/NGC_4808_DR1_catalog.fits"
 SourceFolder="/Users/nate/Dropbox/WALLABY/NGC4808_TR1/NGC_4808_DR1_products/"
 #   And give a name for the folder that will contain the fitting results
 #TargFolder="WRKP_Trial/"
-TargFolder="NGC4808TR1_WRKPTrial_Sept9_2022/"
+TargFolder="NGC4808TR1_WRKPTrial_Nov14_2023/"
 #   Also set the number of bootstraps to make and run
-nBootstraps= 50
+nBootstraps= 1
 #   Set the number of processors to use for different galaxies
 nProcessors=1
 #   And the number of processors to use for bootstraps for each individual galaxy
 nProcessors_Bootstrap=1
-
-#CatName="/Users/nate/Dropbox/WALLABY/HydraDR2Analysis/WALLABY Hydra DR2/WALLABY_PS_Hya_DR2_catalog.xls"
+#   Set the KinTR keyword -- the kinematic team release version
+KinTR="VNGC4808_KinTR1"
