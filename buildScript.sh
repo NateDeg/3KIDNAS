72c7b29b32e6f9f564ff33d84644328fc31f19a6
NateDeg <30897649+NateDeg@users.noreply.github.com>
2026-02-03 13:52:13 -0500

    Updated code to work with new cfitsio package

--- buildScript.sh
+++ buildScript.sh
@@ -1,4 +1,4 @@
 cd third_party/fftw-3.3.8/; make clean; ./configure; make;
-cd ../cfitsio/; make clean; ./configure; make;
+cd ../cfitsio-4.6.3/; make clean; ./configure; make;
 cd ../SoFiA-2-master_2_5_1/; make clean; ./compile.sh; make;
 cd ../../src/; make clean; make
