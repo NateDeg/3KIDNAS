9daee30e4bbb8d7bdb2e92e2685a34d549184e40
NateDeg <30897649+NateDeg@users.noreply.github.com>
2026-04-21 09:26:10 -0400

    Added documentation

--- WRKP_GalaxyFitDriver.py
+++ WRKP_GalaxyFitDriver.py
@@ -1,14 +1,26 @@
+"""
+KIDNAS_GalaxyFitDriver.py
+=======
+
+**Author:** Nathan Deg
+
+**Description:**
+This module is the main driver for fitting a single galaxy with 3KIDNAS.  It takes in a cube + mask and some initial estimates and fits the cube.  It also determines the uncertainties using the flipping bootstrap and will attempt to extract scaling parameters.
+"""
+
 #!/usr/bin/env python3
 from FitDriverScripts import *
-import sys as sys
-import os as os
 import multiprocessing as mp
 from multiprocessing import freeze_support
 
 
-def Main():
+def main():
+    """
+        This is the main function.  It simply calls the driver in FitDriverScripts/FullSingleGalaxyFit.py
+    """
     FSGF.GalaxyFit()
 
+#   This bit is necessary for multiprocessing
 if __name__=="__main__":
     freeze_support()
-    Main()
+    main()
