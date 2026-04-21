e258167d09bab87a263ab856d8cda8b2134c40bd
NateDeg <30897649+NateDeg@users.noreply.github.com>
2026-04-21 09:24:14 -0400

    Added documentation

--- KIDNAS_CatalogueDriver.py
+++ KIDNAS_CatalogueDriver.py
@@ -5,26 +5,24 @@ KIDNAS_CatalogueDriver.py
 **Author:** Nathan Deg
 
 **Description:**
-This module is the main driver for the 3KIDNAS.  
+This module is the main catalogue driver for the 3KIDNAS.  It will take a SoFiA catalogue + data products and fit them all.  It will then take examine the fits and produce a catalogue of more robust/reliable models.
 """
 
 #!/usr/bin/env python3
 from FitDriverScripts import *
 from FullCatalogueDriverScripts import *
-import sys as sys
-import os as os
 import multiprocessing as mp
 from multiprocessing import freeze_support
 
 
-def Main():
-    #GalFitModules={}
+def main():
+    """
+        This is the main function.  It simply calls the driver in FullCatalogueDriverScripts/CatalogueDrivery.py
+    """
     CD.CatalogueDriverMain()
-    
 
-
-
+#   This bit is necessary for multiprocessing
 if __name__=="__main__":
     freeze_support()
-    Main()
+    main()
 
