438f00118cda88ee8a19f316e2962be034798b40
NateDeg <30897649+NateDeg@users.noreply.github.com>
2026-07-07 09:47:24 -0400

    Moved most the geometry functions to the FitDriverScripts file

--- FullCatalogueDriverScripts/InitialGeometryEstimates.py
+++ FullCatalogueDriverScripts/InitialGeometryEstimates.py
@@ -13,6 +13,10 @@ import pandas as pd
 import os.path
 from os import path
 import os as os
+import sys as sys
+
+from . import GenerateAcceptedModelCatalogue as GAMC
+
 
 def GetGeometryEstimates(step,Cat,GalaxyDict,RTDict):
     """
@@ -34,77 +38,15 @@ def GetGeometryEstimates(step,Cat,GalaxyDict,RTDict):
         GalaxyDict : Dictionary
             The updated dictionary now containing the initial geometry estimates.
     """
-    #   Start by getting the PA estimate
-    GalaxyDict['PA_Estimate']=PAEstimate(Cat['kin_pa'][step],Cat['ell_pa'][step])
-    #   There are three built in options for estimating the inclination from the SoFiA catalogue.  They are all based on the SoFiA estimated size and have differing beam corrections
-    if RTDict['IncIniMethod']=='UnCorrected':
-        #   The uncorrected version uses the base minor and major axis size estimate.
-        MinUse2=Cat['ell_min'][step]**2.
-        MajUse2=Cat['ell_maj'][step]**2.
-    elif RTDict['IncIniMethod']=='BeamCorrected':
-        #   The second option is to correct for the beam size.  This assumes the ell_min and ell_maj are approximately the diameter of the galaxy.
-        MinUse2=BeamCorrSize2(Cat['ell_min'][step],GalaxyDict['Beam_Pix'])
-        MajUse2=BeamCorrSize2(Cat['ell_maj'][step],GalaxyDict['Beam_Pix'])
-    elif RTDict['IncIniMethod']=='WALLABY_Like':
-        #   The third option is to correct for the beam size assuming that ell_min and ell_maj are approximately the radii of the galaxy.  This is true for WALLABY-like observations
-        MinUse2=BeamCorrSize2(2.*Cat['ell_min'][step],GalaxyDict['Beam_Pix'])
-        MajUse2=BeamCorrSize2(2.*Cat['ell_maj'][step],GalaxyDict['Beam_Pix'])
-    else:
-        print("No valid inclination estimate method provided.  Ending here")
-        exit()
-
-    #   With the corrected sizes, there are a few adjustments that may be needed.
-    #   First, make sure the major axis used for the size calculation isn't negative
-    if MajUse2 < 0.:
-        MajUse2=GalaxyDict['Beam_Pix']**2.
-    #   If the corrected minor axis is unresolved, set the inclination to be edge on.
-    if MinUse2 <=0.:
-        IncEst=89.
-    else:
-        IncEst=np.arccos(np.sqrt(MinUse2)/np.sqrt(MajUse2))*180./np.pi
-    #   Store the inclination estimate in the GalaxyDict and return it.
-    GalaxyDict['IncEst']=IncEst
+    #   The key functions are actually located in the FitDriverScripts location.  So the first step is getting that location
+    ModDict=GAMC.GetDriverFolder()
+    sys.path.append(ModDict['GalFitterDir'])
+    #   Now that the path has been appended, we can import the geometry estimates.
+    import GeometryEstimates as GE
+    #   The geometry estimates function in GE uses a somewhat simpler dictionary, so we'll set that here.
+    TempDict={'kin_pa':Cat['kin_pa'][step],'ell_pa':Cat['ell_pa'][step],'ell_min':Cat['ell_min'][step],'ell_maj':Cat['ell_maj'][step]}
+    #   Now call the geometry estimate function.
+    GalaxyDict['PA_Estimate'],GalaxyDict['IncEst']=GE.GetGeometryEstimates(TempDict,GalaxyDict['Beam_Pix'],RTDict['IncIniMethod'])
+    #   And return the dictionary.
     return GalaxyDict
-    
-def PAEstimate(kinPA,ellPA):
-    """
-    This function selects which position angle estimate to use for 3KIDNAS. Generally we always want the kinematic PA, but sometimes SoFiA fails to measure this. In such a case, we use the PA from the moment 0 map
-    
-    Parameters
-        ----------
-        kinPA : real
-            The kinematic PA estimate from SoFiA for the galaxy
-        ellPA : real
-            The PA estimate from the galaxy's moment 0 map made by SoFiA
-
-        Returns
-        -------
-        PA_Estimate: real
-            The PA estimate that will be used to initialize the fitting run
-    """
-    #   Check whether the kinematic PA is a nan.  If it is, use ellPA.  Otherwise use kinPA
-    if np.isnan(kinPA):
-        PA_Estimate=ellPA
-    else:
-        PA_Estimate=kinPA
-        
-    return PA_Estimate
-
-def BeamCorrSize2(Size,Beam):
-    """
-    This function calculates the beam corrected size
-    Parameters
-        ----------
-        Size : real
-            The size to be correct
-        ellPA : real
-            The beam size in the same unit as Size
-
-        Returns
-        -------
-        BeamCorrSize2: real
-            The Beam corrected size squared
-    """
-    
-    BeamCorrSize2=Size**2.-Beam**2.
-    return BeamCorrSize2
+  
